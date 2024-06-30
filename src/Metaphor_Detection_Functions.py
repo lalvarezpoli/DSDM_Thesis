b26fea34fd8d4e6f3ca02853ce4c5845ccb8055e
SBoxho <sboxho@yahoo.fr>
2024-06-30 16:47:12 +0200

    Indexing

--- src/Metaphor_Detection_Functions.py
+++ src/Metaphor_Detection_Functions.py
@@ -159,10 +159,9 @@ def regex_matcher_word_order(text, as_words, words_to_match):
                     # Check if any word from as_words appears within the search range
                     if any(words[idx] in as_words for idx in search_indices):
                         selected_sentences.append(sentence)
-                        word = [word for word in words_to_match if word in sentence_lower]
                         break  # Break out of loop after finding the first match
 
-    return selected_sentences, word
+    return selected_sentences
 
 def vectorize(tokens, vocab):
     vector=[]
